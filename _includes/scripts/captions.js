74695e0b629f2c720505ad652987401c8aa36516
Niklas Oberhuber <niklas@oberhuber.org>
2024-09-30 21:47:23 +0000

    fix article list being indented because of caption

--- _includes/scripts/captions.js
+++ _includes/scripts/captions.js
@@ -2,13 +2,15 @@ function addCaptions() {
 	document.querySelectorAll("img").forEach((e) => {
 		let caption = e.alt;
 
-		// Create p element containg the alt-text
-		let captionElement = document.createElement("p");
-		captionElement.innerHTML = caption;
-		captionElement.classList = "caption";
-
-		let parent = e.parentNode;
-		parent.appendChild(captionElement);
+		if (caption != "") {
+			// Create p element containg the alt-text
+			let captionElement = document.createElement("p");
+			captionElement.innerHTML = caption;
+			captionElement.classList = "caption";
+	
+			let parent = e.parentNode;
+			parent.appendChild(captionElement);
+		};
 	});
 };
 
